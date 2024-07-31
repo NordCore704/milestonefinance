0a2131d78a2d15b774e41fc43ff83c4c3033e005
Simon Peter <petersimon0730@gmail.com>
2024-07-31 23:20:17 +0100

    new admin route protection update

--- app/admin/signup/page.js
+++ app/admin/signup/page.js
@@ -1,11 +1,23 @@
-import { AdminSignUp } from '@/exports';
-import React from 'react'
-
+import { AdminSignUp } from "@/exports";
+import React from "react";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function AdminAuthSignUp() {
-    return (
-      <main className="flex flex-col gap-5 bg-scheme-white p-4">
-        <AdminSignUp />
-      </main>
-    );
-  }
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.user?.role === "admin") {
+      router.push("/adminDashboard");
+    } else {
+      router.push("/dashboard");
+    }
+  }, [session]);
+  return (
+    <main className="flex flex-col gap-5 bg-scheme-white p-4">
+      <AdminSignUp />
+    </main>
+  );
+}
