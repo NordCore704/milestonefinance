39efaf3d79ff5e3cadb0a7991188ef7592eda416
Simon Peter <petersimon0730@gmail.com>
2024-08-07 17:30:38 +0100

    corrections made to contact page

--- components/contact/ContactText.jsx
+++ components/contact/ContactText.jsx
@@ -3,30 +3,30 @@ import React from "react";
 
 const ContactText = () => {
   return (
-    <div className="flex flex-col gap-16">
+    <div className="min-h-screen items-start flex flex-col gap-16 sm:w-[40%]">
       <h3 className="sm:text-5xl md:text-6xl lg:text-8xl text-2xl uppercase font-bold text-black">
         Contact
       </h3>
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-2">
-          <h4 className="uppercase text-scheme-purple font-semibold text-sm">
-            Call us
+          <h4 className="uppercase text-scheme-purple font-semibold text-sm sm:text-base">
+          whatsapp contact
           </h4>
-          <p className="underline">+27 88 30 94 17</p>
+          <Link href={'https://wa.me/milestonefinance/447586551360'} className="underline">+447586551360</Link>
         </div>
         <div className="flex flex-col gap-2">
-          <h4 className="uppercase text-scheme-purple font-semibold text-sm">
+          <h4 className="uppercase text-scheme-purple font-semibold text-sm sm:text-base">
             chat support 
           </h4>
-          <Link href={'https://t.me/milestonefinance'} className="underline">t.me/milestonefinance</Link>
+          <Link href={'https://t.me/milestonefinance'} className="underline">milestonefinance</Link>
         </div>
         <div className="flex flex-col gap-2">
-          <h4 className="uppercase text-scheme-purple font-semibold text-sm">
-            visit us
+          <h4 className="uppercase text-scheme-purple font-semibold text-sm sm:text-base">
+            Talk to us
           </h4>
           <div className="flex flex-col">
-            <p className="">123 Design Street,</p>
-            <p className="">Imaginary City, IM 12345</p>
+            {/* <p className=""></p> */}
+            <p className="">For any questions or support, please use the channels provided above. We're here to help!</p>
           </div>
         </div>
       </div>
