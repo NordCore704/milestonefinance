4856b702f072af6ef356be97a5cd1e3e4734af44
Simon Peter <petersimon0730@gmail.com>
2024-12-02 08:14:00 +0100

    dashboard translated

--- components/dashboard/AddInvestmentModal.jsx
+++ components/dashboard/AddInvestmentModal.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
+"use client";
 
-const AddInvestmentModal = ({userData, setShowModal, handleChooseNewPlan }) => {
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const AddInvestmentModal = ({ userData, setShowModal, handleChooseNewPlan }) => {
+  const { t } = useTranslation();
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white rounded-lg p-6 max-w-sm w-full text-center">
-            <h2 className="text-xl font-semibold mb-4">
-              You're on the {userData?.plan} Plan
-            </h2>
-            <p className="mb-6">
-              Would you like to create a new investment plan? Your existing plan
-              will remain active.
-            </p>
-            <div className="flex gap-4 justify-center">
-              <button
-                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
-                onClick={() => setShowModal(false)}
-              >
-                No
-              </button>
-              <button
-                className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
-                onClick={handleChooseNewPlan}
-              >
-                Yes
-              </button>
-            </div>
-          </div>
+      <div className="bg-white rounded-lg p-6 max-w-sm w-full text-center">
+        <h2 className="text-xl font-semibold mb-4">
+          {t("addInvestmentModal.currentPlan", { plan: userData?.plan })}
+        </h2>
+        <p className="mb-6">{t("addInvestmentModal.description")}</p>
+        <div className="flex gap-4 justify-center">
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
+            onClick={() => setShowModal(false)}
+          >
+            {t("addInvestmentModal.noButton")}
+          </button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
+            onClick={handleChooseNewPlan}
+          >
+            {t("addInvestmentModal.yesButton")}
+          </button>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AddInvestmentModal
+export default AddInvestmentModal;
--- components/dashboard/DashboardMain.jsx
+++ components/dashboard/DashboardMain.jsx
@@ -6,14 +6,14 @@ import { Spinner, withAuth, AddInvestmentModal } from "@/exports";
 import { useRouter } from "next/navigation";
 import { fetcher } from "@/lib/fetcher";
 import useSWR from "swr";
-
+import { useTranslation } from "react-i18next";
 
 const DashboardMain = () => {
+  const { t } = useTranslation();
   const { data: session, status } = useSession();
   const router = useRouter();
   console.log(session);
   const [showModal, setShowModal] = useState(false);
-  
 
   const {
     data: userData,
@@ -37,7 +37,7 @@ const DashboardMain = () => {
     }
     return (
       <div className="flex justify-center items-center min-h-screen">
-        <p className="text-2xl text-scheme-purple">Redirecting...</p>
+        <p className="text-2xl text-scheme-purple">{t("Redirecting...")}</p>
       </div>
     );
   }
@@ -47,8 +47,6 @@ const DashboardMain = () => {
     (total, entry) => total + (entry.profitWithdrawn || 0),
     0
   );
- 
-  
 
   const totalBalance =
     Number(userData?.totalProfit) + Number(userData?.amountPaid);
@@ -58,20 +56,22 @@ const DashboardMain = () => {
   console.log(totalProfitWithdrawn, totalWithdrawn);
 
   const plans = {
-    Basic: "Basic",
-    Standard: "Standard",
-    Premium: "Premium",
-    Deluxe: "Deluxe",
+    Basic: t("Basic"),
+    Standard: t("Standard"),
+    Premium: t("Premium"),
+    Deluxe: t("Deluxe"),
   };
 
   const planColors = {
-    Basic: "bg-scheme-purple",
-    Standard: "bg-blue-400",
-    Premium: "bg-orange-400",
-    Deluxe: "bg-red-500",
+    [t('Basic')]: "bg-scheme-purple",
+    [t('Standard')]: "bg-blue-400",
+    [t('Premium')]: "bg-orange-400",
+    [t('Deluxe')]: "bg-red-500",
   };
 
-  const backgroundColor = planColors[userData?.plan] || "bg-scheme-purple";
+
+  const translatedPlan = t(userData?.plan)
+  const backgroundColor = planColors[translatedPlan] || "bg-scheme-purple";
 
   const handleChooseNewPlan = () => {
     setShowModal(false);
@@ -82,27 +82,24 @@ const DashboardMain = () => {
     <section className="flex flex-col gap-10 p-3 sm:p-4 min-h-screen">
       <div className="flex flex-col sm:flex-row justify-between gap-5">
         <div className="flex flex-col gap-2">
-          <p className="text-gray-400">Welcome!</p>
+          <p className="text-gray-400">{t("welcome")}</p>
           <p className="text-2xl font-semibold">{session?.user?.firstName}</p>
-          {/* <p className="text-2xl font-semibold">{session?.user?.email}</p> */}
-          <p className="">
-            Here's a summary of your account. Enjoy your investments!
-          </p>
+          <p className="">{t("summary")}</p>
         </div>
         <div className="flex flex-col gap-2 md:flex-row md:items-center">
           {session.user?.plan === plans[userData?.plan] ? (
-            <button 
-              className={`text-white text-center p-2 rounded-md ${backgroundColor} duration-300 transition-colors`}
+            <button
+              className={`text-white text-center p-2 rounded-md ${backgroundColor} duration-300 transition-colors flex gap-1 hover:bg-gray-500`}
               onClick={() => setShowModal(true)}
             >
-              {userData?.plan} Plan
+              {userData?.plan} <p>{t("Plan")}</p>
             </button>
           ) : (
             <Link
               href={"/dashboard/investment-plans"}
               className="text-white text-center p-2 rounded-md bg-scheme-purple hover:bg-scheme-purpleOne duration-300 transition-colors"
             >
-              Choose a plan!
+              {t("ChoosePlan!")}
             </Link>
           )}
 
@@ -110,39 +107,39 @@ const DashboardMain = () => {
             className="text-white text-center p-2 rounded-md bg-green-500 duration-300 transition-colors hover:bg-green-700 disabled:bg-green-300"
             href={"dashboard/withdraw"}
           >
-            Withdraw
+            {t("withdraw")}
           </Link>
-          {/* <button
-            className="text-white text-center p-2 rounded-md bg-red-500 hover:bg-red-700 duration-300 transition-colors"
-            onClick={() => signOut()}
-          >
-            Logout
-          </button> */}
         </div>
       </div>
 
       {/* ==== modal ==== */}
 
-      {showModal && <AddInvestmentModal setShowModal={setShowModal} userData={userData} handleChooseNewPlan={handleChooseNewPlan}/>}
+      {showModal && (
+        <AddInvestmentModal
+          setShowModal={setShowModal}
+          userData={userData}
+          handleChooseNewPlan={handleChooseNewPlan}
+        />
+      )}
 
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 ">
         {/* === 1 === */}
         <div className="shadow-md rounded-lg p-2 sm:p-3 flex flex-col gap-3 border-b-2 border-scheme-purple">
-          <p className="text-lg">Withdrawable Balance</p>
+          <p className="text-lg">{t("withdrawableBalance")}</p>
           <p className="">
             {Number(userData?.totalProfit).toFixed(2) || "0.00"}
             <span className="text-gray-500"> USD</span>
           </p>
           <div className="flex flex-row justify-between text-center">
             <div className="flex flex-col gap-2">
-              <p className="uppercase text-sm text-gray-600">total balance</p>
+              <p className="uppercase text-sm text-gray-600">{t("totalBalance")}</p>
               <p className="text-sm">
                 {Number(totalBalance) || "0.00"}{" "}
                 <span className="text-gray-500"> USD</span>
               </p>
             </div>
             <div className="flex flex-col gap-2 text-center">
-              <p className="uppercase text-sm text-gray-600">total profit</p>
+              <p className="uppercase text-sm text-gray-600">{t("totalProfit")}</p>
               <p className="text-sm">
                 {Number(userData?.totalProfit).toFixed(2) || "0.00"}{" "}
                 <span className="text-gray-500"> USD</span>
@@ -152,7 +149,7 @@ const DashboardMain = () => {
         </div>
         {/* === 2 === */}
         <div className="shadow-md rounded-lg p-2 sm:p-3 flex flex-col gap-3 border-b-2 border-scheme-darkerGrey">
-          <p className="text-lg">Total Active Investment</p>
+          <p className="text-lg">{t("totalActiveInvestment")}</p>
           <p className="">
             {Number(userData?.amountPaid) || "0.00"}
             <span className="text-gray-500"> USD</span>
@@ -160,7 +157,7 @@ const DashboardMain = () => {
           <div className="flex flex-row justify-between">
             <div className="flex flex-col gap-2">
               <p className="uppercase text-sm text-gray-600">
-                this month(pending inclusive)
+                {t("thisMonth")}
               </p>
               <p className="text-sm">
                 {Number(userData?.amountPaid) || "0.00"}
@@ -171,14 +168,14 @@ const DashboardMain = () => {
         </div>
         {/* ==== 3 ===== */}
         <div className="shadow-md rounded-lg p-2 sm:p-3 flex flex-col gap-5 border-b-2 border-yellow-400">
-          <p className="text-lg">Total Withdrawals</p>
+          <p className="text-lg">{t("totalWithdrawals")}</p>
           <p className="">
             {totalProfitWithdrawn || "0.00"}
             <span className="text-gray-500"> USD</span>
           </p>
           <div className="flex flex-row justify-between">
             <div className="flex flex-col gap-2">
-              <p className="uppercase text-sm text-gray-600">this month</p>
+              <p className="uppercase text-sm text-gray-600">{t("thisMonthNoPending")}</p>
               <p className="text-sm">
                 {totalWithdrawn || "0.00"}
                 <span className="text-gray-500"> USD</span>
@@ -188,16 +185,21 @@ const DashboardMain = () => {
         </div>
       </div>
       <aside className=" sm:w-[40%] flex flex-col gap-5">
-        <h4 className="font-semibold text-lg">Investment History</h4>
+        <h4 className="font-semibold text-lg">{t("investmentHistory")}</h4>
         <Link
           className="bg-gray-200 p-2 rounded-md transition-colors duration-300 hover:text-orange-400 text-green-700 "
           href={"/dashboard/history"}
         >
-          View All
+          {t("viewAll")}
         </Link>
       </aside>
       <div className="">
-      <Link href={'/dashboard/editProfile'} className="font-semibold text-lg hover:text-scheme-purple duration-300 transition-colors">Change Password</Link>
+        <Link
+          href={"/dashboard/editProfile"}
+          className="font-semibold text-lg hover:text-scheme-purple duration-300 transition-colors"
+        >
+          {t("changePassword")}
+        </Link>
       </div>
     </section>
   );
